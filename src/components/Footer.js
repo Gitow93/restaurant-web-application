34acb288a9aa3e18051ddb72d96d03686468fc53
Jorge Jiménez <jorge.jimenezmunoz.93@gmail.com>
2023-09-01 18:21:49 +0200

    add new links and fix errors

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,19 +3,25 @@ import { Link } from "react-router-dom";
 import "../assets/css/footer.scss";
 import logo from "../assets/images/welkhome-logo.png";
 import instalogo from "../assets/images/insta-logo.png";
+import fcblogo from "../assets/images/faceb-logo.png";
 
 
 const Footer = () => {
     return (
 
-        <header className="footer">
+        <footer className="footer">
             <div>
                 <img className="logo" src={logo} alt={logo} />
             </div>
+            <div className="sn_container">
             <Link to="https://www.instagram.com/welkhomeclub/">
-            <img className="sn_logo" src={instalogo} alt={instalogo} />
+                <img className="sn_logo" src={instalogo} alt={instalogo} />
             </Link>
-        </header>
+            <Link to="https://es-la.facebook.com/WELKHOMEclub/">
+                <img className="sn_logo" src={fcblogo} alt={fcblogo}/>
+            </Link>
+            </div>
+        </footer>
 
     );
     };
