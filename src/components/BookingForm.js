e51db4fdec41c02220a90c55cb78622623f415ea
Jorge Jiménez <jorge.jimenezmunoz.93@gmail.com>
2023-09-01 18:24:31 +0200

    create and add funcionalities to BookingForm and Bookings

---
+++ src/components/BookingForm.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const BookingForm = () => {
+  return (
+    <>
+      <h1>HOLA</h1>
+      <p>LOREM LOREM LOREM LOREM </p>
+    </>
+  );
+};
+
+export default BookingForm;
