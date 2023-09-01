db300b13cbb749c620e4a21ce893faf2091a72cc
Jorge Jiménez <jorge.jimenezmunoz.93@gmail.com>
2023-09-01 21:03:06 +0200

    add booking form and style

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -1,11 +1,39 @@
 import React from "react";
+import "../assets/css/bookingForm.scss";
 
 const BookingForm = () => {
   return (
-    <>
-      <h1>HOLA</h1>
-      <p>LOREM LOREM LOREM LOREM </p>
-    </>
+    <div className="booking_form">
+      <h1 className="title">Reserva</h1>
+      <form className="booking_cells" action="procesar_reserva" method="POST">
+        <label htmlFor="fecha">Fecha:</label>
+        <input className="cell" type="date" id="fecha" name="fecha" required />
+
+        <label htmlFor="hora">Hora:</label>
+        <input className="cell" type="time" id="hora" name="hora" />
+
+        <label htmlFor="personas">Personas:</label>
+        <select className="cell" id="personas" name="personas" required>
+          <option value="1">1 persona</option>
+          <option value="2">2 personas</option>
+          <option value="3">3 personas</option>
+          <option value="4">4 personas</option>
+          <option value="5">5 personas</option>
+          <option value="6">6 personas</option>
+          <option value="7">7 personas</option>
+          <option value="8">8 personas</option>
+          <option value="9">9 personas</option>
+          <option value="10">10 personas</option>
+          <option value="11">11 personas</option>
+          <option value="12">12 personas</option>
+          <option value="13">13 personas</option>
+          <option value="14">14 personas</option>
+          <option value="15">15 personas</option>
+        </select>
+
+        <input type="submit" value="Hacer Reserva" />
+      </form>
+    </div>
   );
 };
 
